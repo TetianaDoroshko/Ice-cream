dad0525907b9b69514770de29e77f16679d8e160
Sergey Korobka <nejdancs@gmail.com>
2022-02-23 14:32:46 +0200

    add images webP

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,21 @@
-var swiper = new Swiper(".mySwiper", {
-                pagination: {
-                    el: ".swiper-pagination",
-                    clickable: true,
+var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  on: {
+    init() {
+      this.el.addEventListener('mouseenter', () => {
+        this.autoplay.stop();
+      });
+
+      this.el.addEventListener('mouseleave', () => {
+        this.autoplay.start();
+      });
     },
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        pauseOnMouseEnter:true,
-                }
+  },
 });
-            
